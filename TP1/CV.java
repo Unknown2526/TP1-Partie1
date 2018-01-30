1b6c4c3728ab4849e28ee69f1ce61c968d270da2
Unknown2526 <an_tho_888@live.ca>
2018-01-30 13:06:06 -0500

    CV - départ

---
+++ TP1/CV.java
@@ -0,0 +1,18 @@
+
+public class CV {
+
+	private String nom;
+	private String prenom;
+	private String formation;
+	private double nbAnnee;
+	private String[] competence = new String[2];
+	private String attente;
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+
+}
+
